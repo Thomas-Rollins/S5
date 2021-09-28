cf3a17c6c63278469be17d99538a1fbbe215f3c6
Thomas Rollins <rollins@uoguelph.ca>
2021-09-28 13:05:29 -0400

    implemented initial do_list()

--- aws_s3.py
+++ aws_s3.py
@@ -1,9 +1,9 @@
 import os
-from posixpath import split
-from typing import Any
-from botocore.exceptions import BotoCoreError, ClientError     #Error handling
+from typing import Type
+from botocore.exceptions import BotoCoreError, ClientError, ParamValidationError     #Error handling
 import boto3
 import configparser
+import re
 
 import S5Shell
 
@@ -45,15 +45,15 @@ class aws_s3(S5Shell.s5shell):
         
         return True, 'You are now connected to your S3 storage'
 
-    def __create_S3_resource__(self, session) -> object:
+    def __create_S3_resource__(self, session):
         s3 = session.resource('s3')
         return s3
 
-    def __create_S3_client__(self, session) -> object:
+    def __create_S3_client__(self, session):
         s3 = session.client('s3')
         return s3
 
-    def __bucket_exists__(self, resource, name) ->list[bool, Any]:
+    def __bucket_exists__(self, resource, name) -> list:
         try: 
             resource.meta.client.head_bucket(Bucket=name)
         except ClientError as err:
@@ -63,7 +63,7 @@ class aws_s3(S5Shell.s5shell):
             return [False, 'Invalid Params']
         return [True, None]
 
-    def __object_exists__(self, client, object_path):
+    def __object_exists__(self, client, object_path) -> bool:
         try:
             self.s3_client.head_object(Bucket=self.cloud_cur_bucket, Key=object_path)
             return True
@@ -114,14 +114,11 @@ class aws_s3(S5Shell.s5shell):
         return response['LocationConstraint']
 
     def do_lc_copy(self, args):
-        print('Local Copy:', args)
         success = False
         err = None
-        # print('args:', args)
-
         if args == None:
             success = False
-            err = 'lc_copy takes 2 arguments.'
+            err = 'Invalid number of arguments.'
         else:
             try:
                 local_path, bucket_path = args.split(' ', 1)
@@ -136,50 +133,138 @@ class aws_s3(S5Shell.s5shell):
             else:
                 try:
                     bucket_name, cloud_path = bucket_path.split(":", 1)
-                except IndexError:
+                except IndexError or ValueError:
                     success = False
                 if self.__bucket_exists__(self.s3_resource, bucket_name)[0] :
                     try:
-                        print('local file:', abs_local_path, '\nbucket:', bucket_name, '\nCloud Path:', cloud_path)
                         response = self.s3_client.upload_file(abs_local_path, bucket_name, cloud_path)
                         success = True
                     except ClientError as e:
-                        err_code = err.response['Code']
+                        err_code = e.response['Error']['Code']
                         err = 'Error: ' + err_code
                         success = False
                 else:
                     err = 'Bucket ' + bucket_name + ' does not exist'
                     success = False
         if err != None: 
-            print('Error:', err)
+            print('Error:', err, os.linesep, 'Usage: lc_copy <path of local file> <bucket name>:<full path of s3 object>')
         return 0 if success else 1
         
     def do_cl_copy(self, args):
         print('Cloud copy:', args)
         success = False
+        err = None
 
         if args == None:
             success = False
-        elif ':' in args:
-            try:
-                bucket_path = args.split(':', 1)
-                result = self.__bucket_exists__(self.s3_resource, bucket_path[0])
-                if result[0]:
-                    self.cloud_cur_bucket = bucket_path[0]
-                    self.cloud_wDir = bucket_path[1]
-                    success = True
-            except IndexError:
-                success = False
-
+            err = 'Invalid number of arguments.'
+        else:
+            args_list = args.split(' ')
+        
+        if len(args_list) != 2:
+            success = False
+            'Invalid number of arguments.'
+        else:
+            abs_path = self.get_abs_local_path(args_list[1])
+            if ':' in args_list[0]:
+                try:
+                    bucket_path = args_list[0].split(':', 1)
+                    result = self.__bucket_exists__(self.s3_resource, bucket_path[0])
+                    if result[0]:
+                        try:
+                            response = self.s3_client.download_file(bucket_path[0], bucket_path[1], abs_path)
+                            success = True
+                        except ClientError as e:
+                            err_code = e.response['Error']['Code']
+                            err = 'Error: ' + err_code + ' Failed to download file:', args
+                            success = False
+                    else:
+                        err = 'The bucket', bucket_path[0], 'does not exist or is inaccessible.'
+                except IndexError:
+                    success = False
+        if err != None: 
+            print('Error:', err, os.linesep, 'Usage:', os.linesep, 'cl_copy <bucket name>:<full pathname of S3 File> <path to local file>', os.linesep, 'cl_copy <pathname of S3 File> <path to local file>')
         return 0 if success else 1
 
     def do_create_bucket(self, args):
         print('create a bucket:', args)
-        return False
+        err = None
+        success = False
+        pattern = re.compile('^[a-z0-9-]*$')
+        
+        #default
+        region = 'ca-central-1'
+        bucket_name = None
+        acl = 'private'
+
+        try:
+            args_list = args.split(' ')
+            if '-l' in args_list:
+                region = args_list[args_list.index('-l') + 1]
+                print(region)
+        except IndexError:
+            pass
+        except ValueError:
+            pass
+
+        if len(args_list) > 1:
+            bucket_name = args_list[0]
+        else:
+            bucket_name = args
+
+        if re.match(pattern, bucket_name):
+            try:
+                self.s3_client.create_bucket(ACL=acl, Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': region})
+                success = True
+            except self.s3_client.exceptions.BucketAlreadyExists:
+                pass
+            except ClientError as e:
+                success = False
+                err = e.response['Error']['Code'] + '. ' + e.response['Error']['Message']
+        else:
+            success = False
+            err = 'Invalid bucket name. Bucket names contain only lowercase letters, numbers, and/or hyphens.'
+
+        if err != None:
+            print('Error:', err, os.linesep, 'Usage: create_bucket <bucket name> Optional Params: -l <location>')
+
+        return 0 if success else 1
 
     def do_create_folder(self, args):
         print('create folder', args)
-        return False
+        success = False
+        err = None
+
+        if ':' in args:
+            arg_list = args.split(':')
+            try:
+                folder_bucket = arg_list[0]
+                folder_path = arg_list[1]
+                if not folder_path.endswith('/'):
+                    folder_path += '/'
+                result = True
+            except IndexError:
+                err = 'Invalid arguments.'
+                result = False
+        else:
+            if not self.cloud_cur_bucket == '/':
+                folder_bucket = self.cloud_cur_bucket
+                result, msg = self.__resolve_relative_path__(args)
+        print('folder bucket:', folder_bucket, 'folder path:', folder_path)
+        if result:          
+            try:
+                response = self.s3_client.put_object(Bucket=folder_bucket, Key=folder_path)
+                success = True
+            except ClientError as e:
+                success = False
+                err = e.response['Error']['Code'] + '. ' + e.response['Error']['Message']
+        else:
+            success = False
+            err = msg
+        if err != None:
+            print('Error:', err, os.linesep, 'Usage: create_folder <bucket name>:<full path of folder>')
+
+        return 0 if success else 1
 
     # evaluate if folder exists using s3.Object('bucket', 'object name').load()
     # if at root and there is no : then it must be a bucket
@@ -220,9 +305,9 @@ class aws_s3(S5Shell.s5shell):
                             success = False
                     if success:
                         self.cloud_cur_bucket = bucket_path[0]
-            except IndexError:
+            except IndexError or TypeError:
                 success = False
-        #relative path - change to a resolve_relative_path() function
+        #relative path
         elif self.cloud_cur_bucket != '/':
             status, msg = self.__resolve_relative_path__(args)
             if status:
@@ -272,12 +357,156 @@ class aws_s3(S5Shell.s5shell):
 
     def do_list(self, args):
         print('list buckets', args)
+        success = False
+        err = ''
+        bucket_name = ''
+        key = ''
+        objs = []
+        if args == None or args.rstrip(None) == '':
+            bucket_name = '/'
+        elif ':' in args:
+            args_list = args.split(':', 1)
+            bucket_name = args_list[0]
+            key = args_list[1]
+            if not key.endswith('/'):
+                key += '/'
+        else:
+            bucket_name = args
+            key = ''
+
+        if err == '':
+            try:
+                if bucket_name == '/':
+                    objs = [obj.name for obj in self.s3_resource.buckets.all()]
+                    success = True
+                else:
+                    bucket = self.s3_resource.Bucket(bucket_name)
+                    objs = [obj.key for obj in bucket.objects.filter(Prefix=key)]
+                    print(objs)
+                    success = True
+            except ClientError as e:
+                err = e.response['Error']['Code'] + '. ' + e.response['Error']['Message']
+                success = False
+            except ParamValidationError:
+                err = 'Invalid argumnets'
+                success = False
+
+        if success:
+            if len(objs) > 3:
+                for col1, col2, col3 in zip(objs[::3], objs[1::3], objs[2::3]):
+                    print('{:<25}{:<25}{:<}'.format(col1, col2, col3))
+            else:
+                for obj in objs:
+                    print('{:<25}'.format(obj), end='')
+
+
+        if not success:
+            print('Error:', err, os.linesep, 'Usage: list')
+        return 0 if success else 1
 
     def do_ccopy(self, args):
         print('copy from s3 loc to s3 loc')
+        err = ''
+        success = False
+        src = []
+        dest = []
+        try:
+            args_list = args.split(' ')
+            if len(args_list) != 2:
+                err = 'Invalid number of arguments.'
+            else:
+                if ':' in args_list[0]:
+                    src = args_list[0].split(':', 1)
+                else:
+                    src[0] = self.cloud_cur_bucket
+                    result, msg = self.__resolve_relative_path__(args_list[0])
+                    if result:
+                        src[1] = msg
+                    else:
+                        success = False
+                        err = msg
+                if ':' in args_list[1]:
+                    dest = args_list[1].split(':', 1)
+                else:
+                    dest[0] = self.cloud_cur_bucket
+                    result, msg = self.__resolve_relative_path__(args_list[1])
+                    if result:
+                        dest[1] = msg
+                    else:
+                        success = False
+                        err = msg
+                if not err:
+                    copy_src = {'Bucket' : src[0], 'Key' : src[1]}
+                    try:
+                        self.s3_resource.meta.client.copy(copy_src, dest[0], dest[1])
+                        success = True
+                    except ClientError as e:
+                        err = e.response['Error']['Code'] + '. ' + e.response['Error']['Message']
+        except IndexError or TypeError:
+            err = 'Index or Type error'
+            success = False
+        
+        if not success:
+             print('Error:', err, os.linesep, 'Usage: ccopy <source S3 location of object> <destination S3 location>')
+        return 0 if success else 1
 
     def do_cdelete(self, args):
         print('delete s3 object', args)
+        err = ''
+        success = False
+
+        if ':' in args:
+            args_list = args.split(':', 1)
+            bucket_name = args_list[0]
+            key = args_list[1]
+        else:
+            bucket_name = self.cloud_cur_bucket
+            result, msg = self.__resolve_relative_path__(args)
+            if result:
+                key = msg
+            else:
+                success = False
+                err = msg
+        
+        if err == '':
+            try:
+                bucket = self.s3_resource.Bucket(bucket_name)
+            except ClientError as e:
+                err = e.response['Error']['Code'] + '. ' + e.response['Error']['Message']
+            count = 0
+            for objects in bucket.objects.filter(Prefix=key):
+                count += 1
+            
+            if count <= 1:
+                try:
+                    self.s3_resource.Object(bucket_name, key).delete()
+                    success = True
+                except ClientError as e:
+                    err = e.response['Error']['Code'] + '. ' + e.response['Error']['Message']
+            else:
+                err = args + ' is not empty.'
+            
+        if not success:
+             print('Error:', err, os.linesep, 'Usage: cdelete <path to S3 object>')
+
+        return 0 if success else 1
         
     def do_delete_bucket(self, args):
         print('delete bucket', args)
+        success = False
+        if self.cloud_cur_bucket == args:
+            err = 'Cannot delete your current working bucket (try \'ch_folder /\').'
+            success = False
+        else:
+            try:
+                response = self.s3_client.delete_bucket(Bucket=args)
+                success = True
+            except ClientError as e:
+                err = e.response['Error']['Code'] + ' ' + e.response['Error']['Message']
+                success = False
+        if not success:
+            print('Error:', err, os.linesep, 'Usage: cdelete <path to S3 object>')
+
+        return 0 if success else 1
+
+
